c552979d2366c2c8a7e92acc671ea2f810ae52c3
Electrominch <64524860+Electrominch@users.noreply.github.com>
2021-07-13 01:05:59 +0300

    Initial commit

---
+++ NeuronDots/Class1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuronDotsOnigiri
+{
+    class NeuralNetwork
+    {
+        private double learningRate;
+        private Layer[] layers;
+        private Func<double, double> activation;
+        private Func<double, double> derivative;
+
+        public NeuralNetwork(double learningRate, Func<double, double> activation, Func<double, double> derivative, params int[] sizes)
+        {
+            this.learningRate = learningRate;
+            this.activation = activation;
+            this.derivative = derivative;
+            layers = new Layer[sizes.Length];
+            Random rnd = new Random();
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                int nextSize = 0;
+                if (i < sizes.Length - 1) nextSize = sizes[i + 1];
+                layers[i] = new Layer(sizes[i], nextSize);
+                for (int j = 0; j < sizes[i]; j++)
+                {
+                    layers[i].biases[j] = 1;
+                    for (int k = 0; k < nextSize; k++)
+                    {
+                        layers[i].weights[j][k] = rnd.NextDouble() * 2.0 - 1.0;
+                    }
+                }
+            }
+        }
+
+        public double[] feedForward(double[] inputs)
+        {
+            Array.Copy(inputs, layers[0].neurons, inputs.Length);
+            for (int i = 1; i < layers.Length; i++)
+            {
+                Layer l = layers[i - 1];
+                Layer l1 = layers[i];
+                for (int j = 0; j < l1.size; j++)
+                {
+                    l1.neurons[j] = 0;
+                    for (int k = 0; k < l.size; k++)
+                    {
+                        l1.neurons[j] += l.neurons[k] * l.weights[k][j];
+                    }
+                    l1.neurons[j] += l1.biases[j];
+                    l1.neurons[j] = activation(l1.neurons[j]);
+                }
+            }
+            return layers[layers.Length - 1].neurons;
+        }
+
+        public void backpropagation(double[] targets)
+        {
+            double[] errors = new double[layers[layers.Length - 1].size];
+            for (int i = 0; i < layers[layers.Length - 1].size; i++)
+            {
+                errors[i] = targets[i] - layers[layers.Length - 1].neurons[i];
+            }
+            for (int k = layers.Length - 2; k >= 0; k--)
+            {
+                Layer l = layers[k];
+                Layer l1 = layers[k + 1];
+                double[] errorsNext = new double[l.size];
+                double[] gradients = new double[l1.size];
+                for (int i = 0; i < l1.size; i++)
+                {
+                    gradients[i] = errors[i] * derivative(layers[k + 1].neurons[i]);
+                    gradients[i] *= learningRate;
+                }
+                double[][] deltas = new double[l1.size][];
+                for (int i = 0; i < l1.size; i++)
+                    deltas[i] = new double[l.size]; 
+                for (int i = 0; i < l1.size; i++)
+                {
+                    for (int j = 0; j < l.size; j++)
+                    {
+                        deltas[i][j] = gradients[i] * l.neurons[j];
+                    }
+                }
+                for (int i = 0; i < l.size; i++)
+                {
+                    errorsNext[i] = 0;
+                    for (int j = 0; j < l1.size; j++)
+                    {
+                        errorsNext[i] += l.weights[i][j] * errors[j];
+                    }
+                }
+                errors = new double[l.size];
+                Array.Copy(errorsNext, errors, l.size);
+                double[][] weightsNew = new double[l.weights.Length][];
+                for (int i = 0; i < l.weights.Length; i++)
+                    weightsNew[i] = new double[l.weights[0].Length];
+                for (int i = 0; i < l1.size; i++)
+                {
+                    for (int j = 0; j < l.size; j++)
+                    {
+                        weightsNew[j][i] = l.weights[j][i] + deltas[i][j];
+                    }
+                }
+                l.weights = weightsNew;
+                for (int i = 0; i < l1.size; i++)
+                {
+                    l1.biases[i] += gradients[i];
+                }
+            }
+        }
+
+        public class Layer
+        {
+
+            public int size;
+            public double[] neurons;
+            public double[] biases;
+            public double[][] weights;
+
+            public Layer(int size, int nextSize)
+            {
+                this.size = size;
+                neurons = new double[size];
+                biases = new double[size];
+                weights = new double[size][];
+                for (int i = 0; i < size; i++)
+                    weights[i] = new double[nextSize];
+            }
+
+        }
+    }
+}
---
+++ NeuronDots/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Neural_Network;
+
+namespace NeuronDots
+{
+    public partial class Form1 : Form
+    {
+        private List<ColorPoint> Points = new List<ColorPoint>();
+        private Bitmap Field = new Bitmap(1280 / 8, 720/8);
+        private NeuralNetwork Net;
+        static double Sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+        static double DerivativeSigmoid(double x) => (1 - Sigmoid(x)) * Sigmoid(x);
+        private int Count = 0;
+        private int W = 1280;
+        private int H = 720;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Net = new NeuralNetwork(Sigmoid, DerivativeSigmoid, 
+                                    new NeuronLayer() { NumOfNeurons=2, Bias= true },
+                                    new NeuronLayer() { NumOfNeurons = 60, Bias = true },
+                                    new NeuronLayer() { NumOfNeurons = 3});
+            Net.LearningRatio = 0.05;
+            this.Width = W;
+            this.Height = H;
+            Task.Factory.StartNew(UpdateNN, TaskCreationOptions.LongRunning);
+        }
+
+        Random rnd = new Random();
+        private void UpdateNN()
+        {
+            while (true)
+            {
+                double err = 0;
+                if (Points.Count > 0)
+                {
+                    int k;
+                    for (k = 0; k < Points.Count*100; k++)
+                    {
+                        var p = Points[rnd.Next(Points.Count)];
+                        double nx = (double)p.X / W - 0.5;
+                        double ny = (double)p.Y / H - 0.5;
+                        Net.ForwardPassData(new double[] { nx, ny });
+                        double[] targets = new double[2];
+                        if (p.Type == 1)
+                            Net.SetExpectedOutput(new double[] { 0, 1, 0 });
+                        else if (p.Type == 2)
+                            Net.SetExpectedOutput(new double[] { 0, 0, 1 });
+                        else
+                            Net.SetExpectedOutput(new double[] { 1, 0, 0 });
+                        err += Net.Error;
+                        Net.AdjustWeights();
+                    }
+                    err /= k;
+                }
+
+
+                for (int i = 0; i < W / 8; i++)
+                {
+                    for (int j = 0; j < H / 8; j++)
+                    {
+                        double nx = (double)i / W * 8 - 0.5;
+                        double ny = (double)j / H * 8 - 0.5;
+                        double[] outputs = Net.ForwardPassData(new double[] { nx, ny });
+                        double red = Math.Max(0, Math.Min(1, outputs[0] - outputs[1] - outputs[2] + 0.5));
+                        double green = Math.Max(0, Math.Min(1, outputs[1] - outputs[0] - outputs[2] + 0.5));
+                        double blue = Math.Max(0, Math.Min(1, outputs[2] - outputs[1] - outputs[0] + 0.5));
+                        red = 0.3 + red * 0.5;
+                        green = 0.3 + green * 0.5;
+                        blue = 0.3 + blue * 0.5;
+                        Color c = Color.FromArgb((int)(red*255), (int)(green*255), (int)(blue*255));
+                        lock(Field)
+                            Field.SetPixel(i, j, c);
+                    }
+                }
+
+                Invoke(new Action(() => {
+                    this.Text = string.Format("{0:F8} Count:{1}", err, Count);
+                    this.Refresh();
+                }));
+                Count++;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            lock (Field)
+                g.DrawImage(Field, 0,0, 1280, 720);
+            foreach (var p in Points)
+                g.FillEllipse(p.Type == 1 ? Brushes.Green : (p.Type == 2? Brushes.Blue : Brushes.Red), new Rectangle(p.X, p.Y, 10, 10));
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                Points.Add(new ColorPoint() { X = e.X, Y = e.Y, Type = 1 });
+            else if(e.Button == MouseButtons.Right)
+                Points.Add(new ColorPoint() { X = e.X, Y = e.Y, Type = 2 });
+            else
+                Points.Add(new ColorPoint() { X = e.X, Y = e.Y, Type = 3 });
+            this.Refresh();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Net.SaveToXMLFile(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/save.xml");
+        }
+    }
+
+    struct ColorPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Type { get; set; }
+    }
+}
